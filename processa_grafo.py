510513337e98710379f567bdaef581c698007270
Alessandro Furlan <alessandro.furlan@ape11.com.br>
2020-11-29 15:03:15 -0300

    modularizacao arquivo principal

---
+++ processa_grafo.py
@@ -0,0 +1,15 @@
+import networkx as nx
+import pandas as pd
+df = pd.read_csv('edges_completo.csv')
+
+lista_edges = ["{{source}} {{target}} {'weight': {{weight}}}".replace("{{source}}", str(v[0])).replace("{{target}}", str(v[1])).replace("{{weight}}", str(v[6])) for v in df.values.tolist()]
+
+G = nx.parse_edgelist(lista_edges, nodetype=int)
+list(G.edges(data=True))
+
+nodes = pd.read_csv('nodes_completo.csv')
+nodes = nodes[['Id','Label']]
+
+depara_temp = nodes.to_dict()
+string_to_index = {y:x for x,y in depara_temp["Label"].items()}
+index_to_string = {x:y for x,y in depara_temp["Label"].items()}
--- wfabcaze.py
+++ wfabcaze.py
@@ -1,19 +1,6 @@
 import networkx as nx
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv('edges_completo.csv')
 
-lista_edges = ["{{source}} {{target}} {'weight': {{weight}}}".replace("{{source}}", str(v[0])).replace("{{target}}", str(v[1])).replace("{{weight}}", str(v[6])) for v in df.values.tolist()]
-
-G = nx.parse_edgelist(lista_edges, nodetype=int)
-list(G.edges(data=True))
-
-nodes = pd.read_csv('nodes_completo.csv')
-nodes = nodes[['Id','Label']]
-
-depara_temp = nodes.to_dict()
-string_to_index = {y:x for x,y in depara_temp["Label"].items()}
-index_to_string = {x:y for x,y in depara_temp["Label"].items()}
+from processa_grafo import G, index_to_string, string_to_index
 
 def caminho(node1, node2):
     id_inicio = string_to_index[node1]
